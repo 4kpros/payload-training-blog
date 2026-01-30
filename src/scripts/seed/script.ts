b4a19b61476c4200b35705facc0a40efc490e956
4kpros <prosper.abouar@gmail.com>
2026-01-30 16:01:38 +0100

    feat: admin seeder

---
+++ src/scripts/seed/script.ts
@@ -0,0 +1,12 @@
+import { seedAdmin } from './seeders/admin.seeder'
+
+async function main() {
+    try {
+        await seedAdmin()
+        process.exit(0)
+    } catch (error) {
+        process.exit(1)
+    }
+}
+
+void main()
---
+++ src/scripts/seed/seeders/admin.seeder.ts
@@ -0,0 +1,26 @@
+import { getPayload } from 'payload'
+import config from '@payload-config'
+import { isDuplicateError } from '../lib/is-payload-error'
+import { env } from '@/libs/env'
+
+export async function seedAdmin() {
+    const payload = await getPayload({ config })
+
+    try {
+        console.log('creating admin user...')
+        await payload.create({
+            collection: 'users',
+            data: {
+                email: env.CMS_SEED_ADMIN_EMAIL,
+                password: env.CMS_SEED_ADMIN_PASSWORD,
+            },
+        })
+        console.log('Admin user created')
+    } catch (error) {
+        if (isDuplicateError(error, 'email')) {
+            console.log('Admin user already exists, skipping creation.')
+            return
+        }
+        console.error('Error creating admin user:', JSON.stringify(error, null, 2))
+    }
+}
